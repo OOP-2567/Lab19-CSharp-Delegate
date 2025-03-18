9e8eef31009d6cc27d50c86a509e5a3479be52e9
Thesiri04 <sirirungnapa201@gmail.com>
2025-03-18 21:12:11 +0700

    แบบฝึก

---
+++ Lab19_Ex07/Program.cs
@@ -0,0 +1,7 @@
+﻿Square getSquare = x=> x*x ; // 2. define lambda expression
+
+int num = 9;
+int sqr = getSquare(num);
+System.Console.WriteLine($"Square value of {num} is {sqr}");
+
+delegate int Square(int num);  // 1. declare delegate
