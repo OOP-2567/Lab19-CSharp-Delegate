8e04b41e439573cc6f6a94a82756579ec0364e11
Ohmies <Ohmperapatsingpan.15@gmail.com>
2025-03-19 17:18:15 +0700

    Finish All.

---
+++ Lab19_Ex10/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Func
+Func<int, int, int> add = Add;
+int a = 5, b = 9;
+int c = add(a, b);
+System.Console.WriteLine($"Add({a}, {b}) return {c}");
+
+Func<string, string> convertToUpper = s => s.ToUpper();
+string before = "all Lowercase characters";
+System.Console.WriteLine($"Before = \"{before}\", after = \"{convertToUpper(before)}\"");
+
+static int Add(int a, int b)
+{
+    return a + b;
+}
---
+++ Lab19_Ex7/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Lambda expression
+Square getSquare = x => x * x; // 2. define lambda expression
+
+int num = 9;
+int sqr = getSquare(num);
+System.Console.WriteLine($"Square value of {num} is {sqr}");
+
+delegate int Square(int num);  // 1. declare delegate
